f252454b03814e606b939a34870a3cfb7c4f583a
Thomas Chappel <thomas.chappel4@gmail.com>
2019-11-13 23:32:38 +0100

    Updates

---
+++ precache-manifest.4d3a938f1cab07327861f003807d1653.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4e40231ac270532eb38386155c9f1f",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "c7e7cdc02fb60035e85b",
+    "url": "/todo-list/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "c7e7cdc02fb60035e85b",
+    "url": "/todo-list/static/js/2.6d515966.chunk.js"
+  },
+  {
+    "revision": "61fac310ee7cae66b14e",
+    "url": "/todo-list/static/js/main.df966130.chunk.js"
+  },
+  {
+    "revision": "f93898e5356ea7cd0a6f",
+    "url": "/todo-list/static/js/runtime-main.7417797d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/repository-name/precache-manifest.a15631d7ffc14cf521a18e746ac1fca0.js"
+  "/todo-list/precache-manifest.4d3a938f1cab07327861f003807d1653.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/repository-name/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
